e25e1ae927dd370283bb20c30e9794c46dfce8ae
kiehp <kierra.nelson@aol.com>
2021-09-12 12:57:07 -0400

    initial styling

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -14,3 +14,27 @@ async function logout() {
 
 
 document.querySelector('#logout').addEventListener('click', logout);
+
+async function logoutFormHandler(event) {
+    const email = document.querySelector('#email-logout').value.trim();
+    const password = document.querySelector('#password-logout').value.trim();
+
+    if (email && password) {
+        const response = await fetch('./api/users/logout', {
+            method: 'post', 
+            body: JSON.stringify({
+                email, 
+                password,
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.logout-form').addEventListener('submit', logoutFormHandler);
